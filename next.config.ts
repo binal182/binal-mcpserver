6bf573ff8219c0434ac41991764d5f0ff1831234
binal182 <sbinal182@gmail.com>
2026-01-11 12:58:00 +1100

    updated nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Allow production builds to complete even with ESLint errors
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // Allow production builds to complete even with TypeScript errors
     ignoreBuildErrors: true,
